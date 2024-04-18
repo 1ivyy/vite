e0a0e1d8dc7677154cf0b3d4734441fee65c8345
ivy <ivy@gmail.com>
2024-04-18 10:00:07 +0800

    updated page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import ivy from "./assets/ivy.jpg"
+import ivy from "./assets/ivy2.jpg"
 </script>
 
 <template>
-  <div>
-    <h1>Asivenaty Asut [22FTT1521]</h1>
+  <div class="min-h-screen bg-pink-100 text-gray-800 italic mb-12 text-center sm:text-left px-4 sm:px-0">
+    <h1 class="text-4xl font-bold mb-4 mt-8">Asivenaty Asut [22FTT1521]</h1>
+    <h2 class="text-xl">I am studying Level 5 Diploma in Web Development, in Politeknik Brunei.</h2>
+    <h2 class="text-xl mt-2">I enjoy listening to various genres of music,</h2>
+    <h2 class="text-xl mt-2">and I love to discover new places that serves great coffee.</h2>
 
-    <div>
-      <img :src="ivy" class="w-24 h-24 ml-20" alt="ivy" />
+    <div class="flex justify-center items-center mt-12">
+      <img src="./assets/ivy2.jpg" class="w-48 h-48 sm:w-72 sm:h-72 object-cover border border-gray-300 rounded-full shadow-lg" alt="">
+    </div>
+
+    <div class="mt-12 px-4 sm:px-0 text-center sm:text-left">
+      <h2 class="text-2xl font-bold">CONTACT ME!</h2>
+      <h2>22FTT1521@student.pb.edu.bn</h2>
+      <h2>+673 --- ----</h2>
     </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
