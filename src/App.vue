0d4ff2fbf098f5599be2155c21292a29b3b0b651
ivy <ivy@gmail.com>
2024-04-18 08:32:51 +0800

    added picture

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import ivy from "./assets/ivy.jpg"
 </script>
 
 <template>
   <div>
     <h1>Asivenaty Asut [22FTT1521]</h1>
+
+    <div>
+      <img src="ivy.jpg" alt="">
+    </div>
   </div>
 </template>
 
